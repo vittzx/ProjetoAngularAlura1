fe87a6de989f38f4edd6909e3a4a21aa930cecee
Vitor Cosso <vitorcosso@gmail.com>
2023-11-22 02:40:27 -0300

    Adding footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component"; // tem que importar para a leitura da tag <app-cabecalho> funcionamento do app.component.html
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component"; // tem que importar para a leitura da tag <app-cabecalho> funcionamento do app.component.html
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, CabecalhoComponent]
+    imports: [CommonModule, RouterOutlet, CabecalhoComponent, RodapeComponent]
 })
 export class AppComponent {
   title = 'my-angular-app';
