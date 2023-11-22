6cb639d0943989b863855d5326ae11d6ac08087e
Vitor Cosso <vitorcosso@gmail.com>
2023-11-22 00:54:20 -0300

    Adding imgs and some configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component"; // tem que importar para a leitura da tag <app-cabecalho> funcionamento do app.component.html
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, CabecalhoComponent]
 })
 export class AppComponent {
   title = 'my-angular-app';
