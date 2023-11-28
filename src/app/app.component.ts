13024a9827f86769c5bcde48eeddc4fd0e1a94c0
Vitor Cosso <vitorcosso@gmail.com>
2023-11-28 12:25:51 -0300

    feat: adding criar-pesamento section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
-import { RodapeComponent } from "./componentes/rodape/rodape.component"; // tem que importar para a leitura da tag <app-cabecalho> funcionamento do app.component.html
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
+import { CriarPensamentoComponent } from "./componentes/pesamentos/criar-pensamento/criar-pensamento.component"; // tem que importar para a leitura da tag <app-cabecalho> funcionamento do app.component.html
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, CabecalhoComponent, RodapeComponent]
+    imports: [CommonModule, RouterOutlet, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent, FormsModule]
 })
 export class AppComponent {
   title = 'my-angular-app';
 }
+
---
+++ src/app/componentes/pesamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,27 @@
+import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './criar-pensamento.component.html',
+  styleUrl: './criar-pensamento.component.css'
+})
+export class CriarPensamentoComponent {
+  pensamento = {
+    id: '1',
+    content: 'Learning Angular',
+    autorship: 'Vitor Cosso',
+    model: 'modelo1'
+  }
+
+  criarPensamento(){
+    alert("quote has been created!");
+  }
+
+  cancelarPesamento(){
+    alert("the quote has been canceled!");
+  }
+}
